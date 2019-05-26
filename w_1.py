fbfd584d17b3798ae6bfbbf9cedb57f24ead6359
Kalyani Asthana <kalyaniasthana@gmail.com>
2019-05-26 22:44:55 +0530

    k-means clustering

--- w_1.py
+++ w_1.py
@@ -1,5 +1,7 @@
 import numpy as np
 import math
+import copy
+from collections import Counter
 
 def farthest_first_traversal_input(file):
 	read = open(file)
@@ -52,12 +54,72 @@ def squared_error_distortion(k, m, centers, data):
 	return s/n
 
 
+def form_clusters(centers_assigned, data, k):
+	clusters_with_points = [[] for i in range(k)]
+	for i in range(len(centers_assigned)):
+		j = centers_assigned[i]
+		clusters_with_points[j].append(data[i])
 
+	return clusters_with_points
+
+def average(l):
+    llen = len(l)
+    def divide(x): return x / llen
+    return list(map(divide, map(sum, zip(*l))))
+
+def new_centers(clusters_with_points, k):
+	c = []
+	for l in clusters_with_points:
+		c.append(average(l))
+	return c
+
+def kmeans(k, m, data):
+	centers = data[0:k]
+	centers_assigned = [0 for i in range(len(data))]
+	centers_ = []
+	while True:
+
+		#centers_ = copy.deepcopy(centers)
+		for i in range(len(data)):
+			point = data[i]
+			val = float('Inf')
+			for j in range(len(centers)):
+				s = dist(point, centers[j])
+				if s < val:
+					val = s
+					centers_assigned[i] = j
+		clusters_with_points = form_clusters(centers_assigned, data, k)
+		c = new_centers(clusters_with_points, k)
+		if set(map(tuple,c)) == set(map(tuple,centers)):
+			break
+		else:
+			centers = c
+	return centers
+		
+
+k = 6
+m = 5
+data = farthest_first_traversal_input('kmeans.txt')
+centers = kmeans(k, m, data)
+for i in centers:
+	for k in i:
+		print("%.3f"%k, end = ' ')
+	print('')
+
+
+
+
+
+
+
+
+'''
 k = 5
 m = 2
 centers = farthest_first_traversal_input('centers.txt')
 data = farthest_first_traversal_input('data.txt')
 print(squared_error_distortion(k, m, centers, data))
+'''
 
 '''
 file = 'farthest.txt'
